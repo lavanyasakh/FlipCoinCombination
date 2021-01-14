771df6f615e642d31dd992bf7fca319f15dd4391
Lavanya <lavanyace929@gmail.com>
2021-01-14 19:48:16 +0530

    Added: Doublet combination

--- FlipCoinCombo.sh
+++ FlipCoinCombo.sh
@@ -1,27 +1,48 @@
 #!/bin/bash -x
-IS_HEAD=1
 
+#Doublet Combination
+
+declare -A Store1
+IS_HEADS=1
+IS_TAILS=0
 count1=0
 count2=0
-declare -A Store
-declare -A Percentage
-read -p "Enter a num: " n
-for (( i=1; i<=$n; i++ ))
+count3=0
+count4=0
+read -p "Enter a num for no of times to loop run: " n1
+for (( i=1; i<=$n1; i++ ))
 do
-   FlipCoin=$(( RANDOM%2 ))
-   if [ $FlipCoin -eq $IS_HEAD ]
+	FlipCoin1=$(( RANDOM%2 ))
+	FlipCoin2=$(( RANDOM%2 ))
+	if [[ $FlipCoin1 -eq $IS_HEADS && $FlipCoin2 -eq $IS_HEADS ]]
+	then
+		((count1++))
+		Store1[$i]="HH"
+	elif [[ $FlipCoin1 -eq $IS_HEADS && $FlipCoin2 -eq $IS_TAILS ]]
+	then
+		((count2++))
+		Store1[$i]="HT"
+   elif [[ $FlipCoin1 -eq $IS_TAILS && $FlipCoin2 -eq $IS_HEADS ]]
    then
-	  ((count1++))
-	   Store[$i]=$FlipCoin
-   else 
-	  ((count2++))
-	  Store[$i]=$FlipCoin
-   fi
+      ((count3++))
+      Store1[$i]="TH"
+   elif [[ $FlipCoin1 -eq $IS_TAILS && $FlipCoin2 -eq $IS_TAILS ]]
+   then
+      ((count4++))
+      Store1[$i]="HT"
+	fi
 done
+echo ${Store1[@]}
+	
 Data1=$(( ($count1/$n)*100 ))
 Data2=$(( ($count2/$n)*100 ))
-Percentage[Heads]=$Data1
-Percentage[Tails]=$Data2
-echo $Data1
-echo ${Percentage[@]}
-echo ${Store[@]}
+Data3=$(( ($count3/$n)*100 ))
+Data4=$(( ($count4/$n)*100 ))
+
+declare -A Percentage2
+count=0
+Percentage2[count]=$Data1
+Percentage2[count++]=$Data2
+Percentage2[count++]=$Data3
+Percentage2[count++]=$Data4
+echo ${Percentage1[@]}
